180302375a88acf1a0214896d81c0f1ea70e517a
khumoyun-eddie <eddie.freelance21@gmail.com>
2021-04-03 23:16:18 +0900

    project default configurations

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Button} from '../components';
+
+class App extends Component{
+    render(){
+        return(
+            <div className="container">
+                <Button/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = ()=>{
+    return(
+        <div className='button'>
+            Hi I am a Button
+        </div>
+    )
+}
+
+export default Button;
